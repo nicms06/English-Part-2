21adce0521d67a13cf9cd104b6f0823f12368bb1
nicolas.mingorance <nicolas.mingorance@106-16.info2>
2025-09-29 09:21:10 +0200

    ejercicio 2 completado

--- src/englishpart2/Exercise02.java
+++ src/englishpart2/Exercise02.java
@@ -16,11 +16,14 @@ public class Exercise02 {
 		//Declare the variable age
 		int age;
 		
-		//Declare the variable month
-		int month;
+		//Declare the variable current year
+		int currentYear;
 		
-		//Declare the variable day
-		int day;
+		//Declare the variable year
+		int year;
+		
+		//Create the variable monthPassed
+		int monthPassed;
 	
 		//We ask the user for his age 
 		System.out.print("How old are you?: ");
@@ -28,20 +31,24 @@ public class Exercise02 {
 		//The machine waits for an answer
 		age = sc.nextInt();
 		
-		//We ask for the month he was born 
-		System.out.print("What month were you born? (1 - 12): ");
+		//We ask the user what year is it
+		System.out.print("What year is it?: ");
+		currentYear = sc.nextInt();
 		
-		//The machine waits for an answer
-		month = sc.nextInt();
+		//Create a boolean to check if his birth month has passed
+		System.out.print("Is your birth month over? (1 = yes , 2 = no): ");
+		monthPassed = sc.nextInt();
 		
-		//We create a loop to ensure that a correct value is entered
-		while(month < 1 || month >12) {
-			
-			System.out.print("The month must be between 1 and 2. Please, try again: ");
-			month = sc.nextInt();
-			
+		//We calculate the year of birth
+		year = currentYear - age;
+		
+		//The logic of the exercise
+		if (monthPassed == 1) {
+			System.out.println("Your year of birth is " + year);
 		}
-		
+		else if(monthPassed == 2) {
+			System.out.println("Your year of birth is " + (year-1));
+		}
 		
 		//Close the Scanner
 		sc.close();
